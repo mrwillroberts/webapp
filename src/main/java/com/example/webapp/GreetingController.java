bb21ab7a6686264d017ef06645f89e5a4486ac7f
mrwillroberts <4405078+mrwillroberts@users.noreply.github.com>
2021-02-14 14:04:17 +0000

    Remove cross origin

--- src/main/java/com/example/webapp/GreetingController.java
+++ src/main/java/com/example/webapp/GreetingController.java
@@ -2,7 +2,6 @@ package com.example.webapp;
 
 import java.util.concurrent.atomic.AtomicLong;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,7 +12,6 @@ public class GreetingController {
 	private static final String template = "Hello, %s!";
 	private final AtomicLong counter = new AtomicLong();
 
-	@CrossOrigin(origins = "http://localhost:1234")
 	@GetMapping("api/greeting")
 	public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
 		return new Greeting(counter.incrementAndGet(), String.format(template, name));
